029c26d970bac2141eb8a2855b2a57fa0d9d8db9
Daniel James <danieldragonhatcher@gmail.com>
2021-06-18 15:26:23 -0700

    Added LexTokens

---
+++ src/commonMain/kotlin/com/drjcoding/plow/lexer/LexToken.kt
@@ -0,0 +1,10 @@
+package com.drjcoding.plow.lexer
+
+/**
+ * Represents a lexical token of text in a source file.
+ *
+ * @property type The microsyntactic class of the token.
+ * @property text The literal text that makes up the token.
+ * @property location The location in the source file that the token was found.
+ */
+data class LexToken(val type: LexTokenType, val text: String, val location: SourceFileLocation)
