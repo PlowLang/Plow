17cf430262f58fc827c94ff0b7e2609fb1cc6a48
Daniel James <danieldragonhatcher@gmail.com>
2021-06-19 19:57:11 -0700

    Enhancments to CharStream

--- src/commonMain/kotlin/com/drjcoding/plow/lexer/CharacterStream.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/lexer/CharacterStream.kt
@@ -12,14 +12,16 @@ class CharacterStream(private val text: String) {
      * Advances the stream one character by incrementing [currentCharPosition] and updates [currentLineNumber] and
      * [currentColumnNumber].
      */
-    private fun advanceStream() {
-        if (safePeek() == '\n') {
-            currentLineNumber++
-            currentColumnNumber = 1
-        } else {
-            currentColumnNumber++
+    private fun advanceStream(times: Int = 1) {
+        repeat(times) {
+            if (safePeek() == '\n') {
+                currentLineNumber++
+                currentColumnNumber = 1
+            } else {
+                currentColumnNumber++
+            }
+            currentCharPosition++
         }
-        currentCharPosition++
     }
 
     /**
@@ -41,10 +43,29 @@ class CharacterStream(private val text: String) {
     fun safePeek(): Char? = if (isEOF) null else text[currentCharPosition]
 
     /**
+     * Returns the next [n] characters of the stream without advancing the stream or null if that many characters
+     * would extend beyond the length of the stream.
+     */
+    fun safePeek(n: Int): String? =
+        if (currentCharPosition + n - 1 in text.indices) null else
+            text.substring(currentCharPosition, currentCharPosition + n)
+
+    /**
      * Returns the next character in the stream without advancing the stream. If the stream has been exhausted ([isEOF])
      * then the function throws a [CharacterStreamAccessedAfterExhaustedException].
      */
     fun peek(): Char = if (isEOF) throw CharacterStreamAccessedAfterExhaustedException() else text[currentCharPosition]
+
+    /**
+     * Returns the next [n] characters of the stream without advancing the stream. If [n] characters would extend
+     * beyond the end of the stream the function throws a [CharacterStreamAccessedAfterExhaustedException].
+     */
+    fun peek(n: Int): String = safePeek(n) ?: throw CharacterStreamAccessedAfterExhaustedException()
+
+    /**
+     * Returns if true if [text] is the next text in the stream.
+     */
+    fun textIsNext(text: String): Boolean = safePeek(text.length) == text
 
     /**
      * Returns the next character in the stream and advance the stream or returns null if the stream has been exhausted
@@ -66,6 +87,16 @@ class CharacterStream(private val text: String) {
         val char = peek()
         advanceStream()
         return char
+    }
+
+    /**
+     * Returns the next [n] characters in the stream and advances the stream [n] characters. If the stream is exhausted
+     * by this many characters then the function throws a [CharacterStreamAccessedAfterExhaustedException].
+     */
+    fun pop(n: Int): String {
+        val str = peek(n)
+        advanceStream(n)
+        return str
     }
 }
 
