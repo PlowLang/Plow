50f0c36a19e3eeb41983aa7b49368b0e51b9356e
Daniel James <danieldragonhatcher@gmail.com>
2021-06-20 17:52:10 -0700

    Added ability to convert SourceFileRange to PlowIssueTextRange.SameFile

--- src/commonMain/kotlin/com/drjcoding/plow/source_abstractions/SourceFileRange.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/source_abstractions/SourceFileRange.kt
@@ -1,5 +1,7 @@
 package com.drjcoding.plow.source_abstractions
 
+import com.drjcoding.plow.errors.PlowIssueTextRange
+
 /**
  * Represents a range of characters within a source file.
  *
@@ -7,3 +9,8 @@ package com.drjcoding.plow.source_abstractions
  * @property length The number of characters in the range.
  */
 data class SourceFileRange(val start: SourceFileLocation, val length: Int)
+
+/**
+ * Converts this [SourceFileRange] to a [PlowIssueTextRange.SameFile].
+ */
+fun SourceFileRange.toPlowIssueTextRange() = PlowIssueTextRange.SameFile(this)
