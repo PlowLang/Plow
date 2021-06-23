025584d4ab2258db7ec2cb82a4502559c08729ba
Daniel James <danieldragonhatcher@gmail.com>
2021-06-23 14:52:35 -0700

    Added function runCatchingExceptionsAsPlowResult

--- src/commonMain/kotlin/com/drjcoding/plow/issues/PlowResult.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/issues/PlowResult.kt
@@ -20,3 +20,22 @@ sealed class PlowResult<T> {
      */
     class Error<T>(val issues: Collection<PlowIssue>) : PlowResult<T>()
 }
+
+
+/**
+ * Runs the given block and catches any exceptions that occur. Returns a [PlowResult] based on the exceptions that
+ * occur.
+ * - If a [PlowFatalIssue] is thrown then a [PlowResult.Error] is returned containing the fatal issue.
+ * - If any other exception is thrown then a [PlowResult.Error] is returned containing an [InternalCompilerError].
+ * - If no exceptions are thrown then a [PlowResult.Ok] is returned containing the result of [run].
+ */
+inline fun <T> runCatchingExceptionsAsPlowResult(run: () -> T): PlowResult<T> =
+    try {
+        val result = run()
+        PlowResult.Ok(result)
+    } catch (pfi: PlowFatalIssue) {
+        PlowResult.Error(listOf(pfi))
+    } catch (e: Exception) {
+        val ice = InternalCompilerError(e)
+        PlowResult.Error(listOf(ice))
+    }
