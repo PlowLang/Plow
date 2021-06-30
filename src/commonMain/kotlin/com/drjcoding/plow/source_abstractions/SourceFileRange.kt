c0ab86d61b614b21bde56baf2c86176688e9d9a3
Daniel James <danieldragonhatcher@gmail.com>
2021-06-30 12:48:14 -0700

    Added functions to manipulate SourceFileRanges

--- src/commonMain/kotlin/com/drjcoding/plow/source_abstractions/SourceFileRange.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/source_abstractions/SourceFileRange.kt
@@ -8,7 +8,19 @@ import com.drjcoding.plow.issues.PlowIssueTextRange
  * @property start The first character of the range (inclusive).
  * @property length The number of characters in the range.
  */
-data class SourceFileRange(val start: SourceFileLocation, val length: Int)
+data class SourceFileRange(val start: SourceFileLocation, val length: Int) {
+
+    /**
+     * Returns the two [SourceFileRange]s concatenated together by adding their lengths. This only works for two ranges
+     * that are adjacent with no gaps in between.
+     */
+    operator fun plus(other: SourceFileRange) = SourceFileRange(this.start, this.length + other.length)
+
+    /**
+     * Returns a new [SourceFileRange] with the same start location but a length [additionalLength] longer.
+     */
+    fun plusLength(additionalLength: Int) = SourceFileRange(this.start, this.length + additionalLength)
+}
 
 /**
  * Converts this [SourceFileRange] to a [PlowIssueTextRange.SameFile].
