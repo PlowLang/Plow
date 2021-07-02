38af2477f0e727dcf275534d4497ed82148ec305
Daniel James <danieldragonhatcher@gmail.com>
2021-07-02 16:02:41 -0700

    Change PlowResult<T> to PlowResult<out T>

--- src/commonMain/kotlin/com/drjcoding/plow/issues/PlowResult.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/issues/PlowResult.kt
@@ -4,21 +4,21 @@ package com.drjcoding.plow.issues
  * The result of any Plow operation that could throw an error. [T] is the type of the data the operation is expected to
  * produce.
  */
-sealed class PlowResult<T> {
+sealed class PlowResult<out T> {
     /**
      * The operation succeeded.
      *
      * @property result The data produced by the operation.
      * @property issues Any [PlowNonfatalIssue]s produced by the operation.
      */
-    class Ok<T>(val result: T, val issues: Collection<PlowNonfatalIssue> = listOf()) : PlowResult<T>()
+    class Ok<out T>(val result: T, val issues: Collection<PlowNonfatalIssue> = listOf()) : PlowResult<T>()
 
     /**
      * The operation failed.
      *
      * @property issues The [PlowIssue]s produced by the operations. (Non-empty).
      */
-    class Error<T>(val issues: Collection<PlowIssue>) : PlowResult<T>() {
+    class Error<out T>(val issues: Collection<PlowIssue>) : PlowResult<T>() {
         constructor(issue: PlowIssue) : this(listOf(issue))
     }
 
