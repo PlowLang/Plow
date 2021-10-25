8e96ee640d342485b22d281fe9d0cbcd69d0f71a
Daniel James <danieldragonhatcher@gmail.com>
2021-10-24 18:16:33 -0700

    added documentation

--- src/commonMain/kotlin/com/drjcoding/plow/parser/ast_nodes/ASTNode.kt
+++ src/commonMain/kotlin/com/drjcoding/plow/parser/ast_nodes/ASTNode.kt
@@ -2,6 +2,11 @@ package com.drjcoding.plow.parser.ast_nodes
 
 import com.drjcoding.plow.parser.cst_nodes.CSTNode
 
+/**
+ * A node in the AST. This is made from the CST. In contrast to the CST the AST does not include syntactical elements,
+ * only the semantically important pieces. It is still, however, directly related to the syntax of the language. Very
+ * little, if any, processing is done between the CST and the AST, only removal of punctuation.
+ */
 abstract class ASTNode {
     abstract val underlyingCSTNode: CSTNode
 }
